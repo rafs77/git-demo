6976f9931d22d0447763fa9e20c12dbb664df16e
Rafael A Flores <rafs70@gmail.com>
2017-09-27 18:18:50 -0500

    Add execute

--- TestGitJava/src/org/fs/tst/AppInit.java
+++ TestGitJava/src/org/fs/tst/AppInit.java
@@ -5,13 +5,16 @@ public class AppInit {
 	public AppInit(){
 		
 	}
+	public void execute(){
+		
+	}
 	/**
 	 * Initial entry point
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		AppInit app = new AppInit();
-		
+		app.execute();
 
 	}
 
