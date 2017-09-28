cd6462e716fe2739e8cb8565fede08e4edc9e5bb
Rafael A Flores <rafs70@gmail.com>
2017-09-28 15:45:29 -0500

    Base for TestGitBProj

---
+++ TestGitBProj/src/org/fs/btst/TestInit.java
@@ -0,0 +1,32 @@
+package org.fs.btst;
+
+/**
+ * Launch business test
+ * @author rafs
+ *
+ */
+public class TestInit {
+
+	/**
+	 * Constructor
+	 */
+	public TestInit(){
+		
+	}
+	/**
+	 * Init business test execution
+	 */
+	public void execute(){
+		
+	}
+	/**
+	 * main entry
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestInit app = new TestInit();
+		app.execute();
+
+	}
+
+}
