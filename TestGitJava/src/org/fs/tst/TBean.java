8ee22af0bab38a488b94966b71c60323fc5d99f5
Rafael A Flores <rafs70@gmail.com>
2017-09-28 14:17:36 -0500

    Add bean testA

---
+++ TestGitJava/src/org/fs/tst/TBean.java
@@ -0,0 +1,25 @@
+package org.fs.tst;
+/**
+ * Bean de prueba 
+ * @author rafs
+ *
+ */
+public class TBean {
+   private String nombre;
+   private String localidad;
+   
+   
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getLocalidad() {
+	return localidad;
+}
+public void setLocalidad(String localidad) {
+	this.localidad = localidad;
+}
+   
+}
